ea2bcc244ac3b3059395275763a444b7a2b071cb
Chris1234567899 <cerlic@web.de>
2024-03-05 13:31:00 -0500

    base test 2

--- croppie/vite.config.js
+++ croppie/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/croppie_web/',
+  base:'/croppie_web/dist/',
   plugins: [react()],
 })
